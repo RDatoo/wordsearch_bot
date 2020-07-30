1caccb3969ebce97ed2705b17fcf81535f54054a
RDatoo <rayhaandatoo@outlook.com>
2020-07-30 00:06:34 -0400

    fixed edge case issue, single word now fully searchable

--- search_bot.py
+++ search_bot.py
@@ -1,5 +1,3 @@
-from search_functions import*
-
 filename = "puzzle.txt"
 seperater = "\t"
 f = open(filename, "r")
@@ -18,10 +16,12 @@ for i in row_list:
 height = len(row_list)
 width = len(row_list[0].split(seperater))
 directions = [-width -1, -width, -width +1, -1, 1, width -1, width, width + 1]
+edges = []
+for i in range(len(letter_list)):
+    if i < width or i%width == 0 or i%width == width-1 or i > len(letter_list)-width-1:
+        edges.append(i)
 
-
-
-target_word = "JON"
+target_word = "ASSIGNMENT"
 current_letter = 0
 target_word_list = []
 
@@ -33,14 +33,18 @@ for i in range(len(letter_list)):
         first_letter.append(i)
 
 for i in first_letter:
-    x = 0 
+    x = 0
+    y = 0
     blist = [i + directions[0], i + directions[1], i + directions[2], i + directions[3], i + directions[4], i + directions[5], i + directions[6], i + directions[7]]
-
+    for b in blist:
+        if b > len(letter_list)-1:
+            blist[y] = 1
+        y += 1
     for j in blist:
         if letter_list[j] == target_word[1]:
             target_word_list.append((i, x))
         x += 1
-    
+
 
 
 for beep in target_word_list:
@@ -49,11 +53,20 @@ for beep in target_word_list:
         word_code.append(word_code[-1] + directions[beep[1]])
     word = ""
     x = 0
+    y = 0
+    
+    for j in word_code:
+        if j > len(letter_list)-1:
+            word_code[y] = 1
+        y += 1
+    
     for i in word_code:
         word += letter_list[word_code[x]]
         x += 1
+    out_of_bounds = False
+    for i in word_code[1:-1]:
+        if i in edges:
+            out_of_bounds = True
     if word == target_word:
         print(word_code)
-
-    
 
